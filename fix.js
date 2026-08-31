a2bc373fc00a1985baa87a5061b26d78e7848d7a
zorrokurro <layjack102389@gmail.com>
2026-08-31 18:31:56 +0800

    Add minimal SVG placeholder image to all cards

--- fix.js
+++ fix.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const cards = JSON.parse(fs.readFileSync('C:/Users/layja/obsidian/worldbuilding/card-game/tcg-arena/cards.json', 'utf8'));
 
-// Replace all images with tiny placeholder
-const placeholder = 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="250" height="350"%3E%3Crect width="250" height="350" rx="12" fill="%232a2a4a"/%3E%3Ctext x="125" y="180" text-anchor="middle" fill="%23fff" font-size="14"%3ECard%3C/text%3E%3C/svg%3E';
+// Minimal 1x1 transparent PNG as data URI
+const img = 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22250%22%20height%3D%22350%22%3E%3Crect%20width%3D%22250%22%20height%3D%22350%22%20fill%3D%22%232a2a4a%22%2F%3E%3C%2Fsvg%3E';
 
 for (const card of Object.values(cards)) {
-  card.face.front.image = placeholder;
+  card.face.front.image = img;
 }
 
-// Pretty print
 fs.writeFileSync('C:/Users/layja/obsidian/worldbuilding/card-game/tcg-arena/cards.json', JSON.stringify(cards, null, 2), 'utf8');
-console.log('File size:', fs.statSync('C:/Users/layja/obsidian/worldbuilding/card-game/tcg-arena/cards.json').size, 'bytes');
+console.log('Size:', fs.statSync('C:/Users/layja/obsidian/worldbuilding/card-game/tcg-arena/cards.json').size);
